708fd48c26498fbfd020ea69731a32b570140136
saifaldin14 <saifaldindeveloper@gmail.com>
2021-01-19 13:05:14 -0500

    Details created

---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
+
+const Details = () => {
+  return (
+    <Card>
+      <CardHeader />
+      <CardContent>
+        <Typography />
+      </CardContent>
+    </Card>
+  )
+}
+
+export default Details
