7a9a4d36cfb5e9e732c3458cb126fbd24e1f94ba
saifaldin14 <saifaldindeveloper@gmail.com>
2021-01-23 10:37:54 -0500

    Update Form.jsx

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -80,9 +80,42 @@ const NewTransactionForm = () => {
   const selectedCategories = formData.type === 'Income' ? incomeCategories : expenseCategories;
 
   return (
-    <div>
-
-    </div>
+    <Grid container spacing={2}>
+      <Snackbar open={open} setOpen={setOpen} />
+      <Grid item xs={12}>
+        <Typography align="center" variant="subtitle2" gutterBottom>
+          {segment ? (
+            <div className="segment">
+              {segment.words.map((w) => w.value).join(" ")}
+            </div>
+          ) : null}
+        </Typography>
+      </Grid>
+      <Grid item xs={6}>
+        <FormControl fullWidth>
+          <InputLabel>Type</InputLabel>
+          <Select value={formData.type} onChange={e => setFormData({...formData, type: e.target.value})}>
+            <MenuItem value="Income">Income</MenuItem>
+            <MenuItem value="Expense">Expense</MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
+      <Grid item xs={6}>
+        <FormControl fullWidth>
+          <InputLabel>Category</InputLabel>
+          <Select value={formData.category} onChange={e => setFormData({...formData, category: e.target.value})}>
+            {selectedCategories.map(c => <MenuItem key={c.type} value={c.type}>{c.type}</MenuItem>)}
+          </Select>
+        </FormControl>
+      </Grid>
+      <Grid item xs={6}>
+        <TextField type="number" label="Amount" value={formData.amount} onChange={e => setFormData({...formData, amount: e.target.value})} fullWidth />
+      </Grid>
+      <Grid item xs={6}>
+        <TextField type="date" label="Date" value={formData.date} onChange={e => setFormData({...formData, date: formatDate(e.target.value)})} fullWidth />
+      </Grid>
+      <Button className={classes.button} variant="outlined" color="primary" fullWidth onClick={createTransaction}>Create</Button>
+    </Grid>
   )
 }
 
